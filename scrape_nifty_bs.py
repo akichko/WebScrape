4170a31b72920b1488ea5ec91633afc1e9e2ecd3
akichko <s.akihiko@gmail.com>
2021-08-10 21:49:09 +0900

    at home page

--- scrape_nifty_bs.py
+++ scrape_nifty_bs.py
@@ -9,62 +9,18 @@ class WebScrapeNiftyBs(ws.WebScrapeBs):
         ua = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36'
         self.headers = {'User-Agent': ua}
   
+
     def __scrape_list_page(self, element):
         nayoses = element.select(".nayose")
         for nayose in nayoses:
-            nayose_url = self.__get_nayose_url(nayose)
-            print(nayose.get('data-name'))
-            print(nayose_url)
-
-            record = self.__get_simple_record(nayose)
-            #ページ種別で解析切替
-            detail_record = self.get_record_by_url(nayose_url)
-            if detail_record != None:
-                record.update(detail_record)
-                
-            self.df = self.df.append(record, ignore_index=True)
-            record['url'] = nayose_url
-
-    
-    def __get_nayose_url(self, nayose):
-        a = nayose.select_one(".nayose_head > p > a")
-        return a.get('href')
-
-    def __get_sub_page_list(self, element):
-        nayoses = element.select(".nayose")
-        for nayose in nayoses:
-            print(nayose.get('data-name'))
-            yield self.__get_nayose_url(nayose)
-
-            #links = nayose.cssselect(".nayose_head p a")
-            #if len(links) > 0:
-            #    yield links[0].get('href')
-
-    def get_record_by_elem(self, element): #element:detail page
-        keys = element.select('#detailInfoTable tr th')
-        if len(keys) == 0:
-            return None
-        values = element.select('#detailInfoTable tr td')
-        values = [x.text if x.text is not None else '' for x in values]
-        record = {keys[i].text: values[i].strip() for i in range(len(keys))}
-        if len(element.select('.titleMain .name')) > 0:
-            record['名前'] = element.select('.titleMain .name')[0].text
-        if len(element.select('.otherPrice')) > 0:
-            record['管理費等'] = element.select('.otherPrice')[0].text
-        return record
-
-    def __get_simple_record(self, nayose):
-        keys = nayose.select(".itemContent dt")
-        values = nayose.select(".itemContent dd")
-        record = {keys[i].text: values[i].text.strip() for i in range(len(keys))}
-        return record
-       
+            #pass
+            self.__get_record_by_nayose(nayose)            
 
     def __get_next_list_page_url(self, element): #override
         elems = element.select(".pageNation li.mg3 a")
         for a in elems:
             if a.text == "次へ>":
-                return a.href('href')
+                return a.get('href')
         #if len(links) == 0:
         #    return None
         #if links[-1].text != "次へ>":
@@ -74,7 +30,86 @@ class WebScrapeNiftyBs(ws.WebScrapeBs):
         #return link_url
 
 
+    def __get_record_by_nayose(self, nayose):
+        
+        nayose_url = self.__get_nayose_url(nayose)
+        print(nayose.get('data-name'))
+        print(nayose_url)
+
+        record = self.__get_simple_record(nayose)
+        elem_nayose_page = self.get_element_by_url(nayose_url)
+
+        #ページ種別で解析切替
+        elem_link = nayose.select_one(".shinchiku_manshion .data .company")
+        if elem_link.select('.athomef'):
+            detail_record = self.__get_record_by_detail_page_2(elem_nayose_page)
+            print('at home')
+        elif elem_link.select('.yahoof'):
+            detail_record = self.__get_record_by_detail_page(elem_nayose_page)
+            print('yahoo')
+        else:
+            detail_record = self.__get_record_by_detail_page(elem_nayose_page)
+            print('other')
+            pass
+
+        if detail_record != None:
+            detail_record.update(record)
+            record = detail_record
+            
+        record['url'] = nayose_url
+        record['cate'] = nayose.select_one("span.cate").text
+
+        record['pref'] = self.pref
+        record['area'] = self.area
+        self.df = self.df.append(record, ignore_index=True)
+    
+    def __get_nayose_url(self, nayose):
+        a = nayose.select_one(".nayose_head > p > a")
+        return a.get('href')
+
+    def __get_simple_record(self, nayose):
+        keys = nayose.select(".itemContent dt")
+        values = nayose.select(".itemContent dd")
+        record = {keys[i].text: values[i].text.strip() for i in range(len(keys))}
+        return record
+
+    def __get_record_by_detail_page(self, element): #element:detail page
+        keys = element.select('#detailInfoTable tr th')
+        if len(keys) == 0:
+            return None
+        values = element.select('#detailInfoTable tr td')
+        #values = [x.text if x.text is not None else '' for x in values]
+        record = {keys[i].text: values[i].text.strip() for i in range(len(keys))}
+        if len(element.select('.titleMain .name')) > 0:
+            record['name'] = element.select('.titleMain .name')[0].text
+        if len(element.select('.otherPrice')) > 0:
+            record['othercost'] = element.select('.otherPrice')[0].text
+        record['site'] = 'nifty'
+        return record
+
+    def __get_record_by_detail_page_2(self, element): #element:detail page
+        #別ページ
+        elem_iframe = element.select_one('iframe#itemDetailFrame')
+        athome_url = elem_iframe.get('src')
+        elem_athome_page = self.get_element_by_url(athome_url)
+
+        keys = elem_athome_page.select('.wrapLeftnoMap tr th')
+        if len(keys) == 0:
+            return None
+        values = elem_athome_page.select('.wrapLeftnoMap tr td')
+        record = {keys[i].text: values[i].text.strip() for i in range(len(keys))}
+
+        record['name'] = record['建物名・部屋番号']
+        record['site'] = 'at home'
+
+        return record
+
+
     def scrape_all(self, url): #override
+        print("start")
+        self.clear_df()
+        print(url)
+
         body = self.get_element_by_url(url)
 
         self.paging_exe(body, self.__scrape_list_page, self.__get_next_list_page_url)
@@ -91,7 +126,19 @@ class Controller:
     east = ['katsushikaku','edogawaku','taitoku','kotoku','sumidaku','chuoku']
     south = ['minatoku','shinagawaku','otaku','meguroku','chiyodaku']
     kanto['tokyo'] = {'e':east, 'w':west, 's':south, 'n':north}
-    kanto['tokyo']
+
+    mito = ['mitoshi','hitachinakashi']
+    kanto['ibaraki'] = {'mito':mito}
+
+    north = ['yokohamashitsurumiku','yokohamashikanagawaku','yokohamashinishiku', \
+            'yokohamashinakaku','yokohamashiminamiku','yokohamashihodogayaku', \
+            'yokohamashikohokuku','yokohamashiaobaku','yokohamashitsuzukiku']
+    south = ['yokohamashiisogoku','yokohamashikanazawaku','yokohamashitotsukaku', \
+            'yokohamashikonanku','yokohamashiasahiku','yokohamashimidoriku', \
+            'yokohamashiseyaku','yokohamashisakaeku','yokohamashiizumiku']
+    kanto['kanagawa'] = {'n':north, 's':south}
+
+
 
     def __init__(self):
         self.wsn = WebScrapeNiftyBs()
@@ -111,19 +158,41 @@ class Controller:
             + "&b10=20&b6=15"
         return url
 
-    def start_scraping(self, url):
+    def start_scraping(self, pref, area, max_price = 20000000):
+        self.wsn.pref = pref
+        self.wsn.area = area
+        url = self.make_url(pref, area, max_price)
         return self.wsn.scrape_all(url)
 
 
 #Main
 controller = Controller()
+df = pd.DataFrame()
 
-df_e = controller.start_scraping(controller.make_url('tokyo', 'e', 20000000))
-df_e['area'] = 'east'
-df_s = controller.start_scraping(controller.make_url('tokyo', 's', 20000000))
-df_s['area'] = 'south'
-df = pd.concat([df_e,df_s])
+#tokyo
+#df_tmp = controller.start_scraping('tokyo', 'e', 20000000)
+#df = pd.concat([df,df_tmp])
+#df_tmp = controller.start_scraping('tokyo', 's', 20000000)
+#df = pd.concat([df,df_tmp])
+#df_tmp = controller.start_scraping('tokyo', 'w', 20000000)
+#df = pd.concat([df,df_tmp])
+#df_tmp = controller.start_scraping('tokyo', 'n', 20000000)
+#df = pd.concat([df,df_tmp])
 
 #df = pd.concat([df_w,df_e,df_n,df_s])
-df.to_csv("nifty_records_tokyo.csv", index=False)   
 
+#df.to_csv("nifty_records_tokyo.csv", index=False)   
+
+#ibaraki
+df_tmp = controller.start_scraping('ibaraki', 'mito', 20000000)
+df = pd.concat([df,df_tmp])
+#df.to_csv("nifty_records_ibaraki.csv", index=False)   
+
+#yokohama
+#df_tmp = controller.start_scraping('kanagawa', 's', 20000000)
+#df = pd.concat([df,df_tmp])
+#df_tmp = controller.start_scraping('kanagawa', 'n', 20000000)
+#df = pd.concat([df,df_tmp])
+#df.to_csv("nifty_records_yokohama.csv", index=False)   
+
+df.to_csv("nifty_records_all.csv", index=False)   
--- webscrapeBs.py
+++ webscrapeBs.py
@@ -1,5 +1,5 @@
 from abc import ABCMeta, abstractmethod
-import webscrape as ws
+#import webscrape as ws
 import requests
 #import lxml.html
 from bs4 import BeautifulSoup
@@ -7,14 +7,18 @@ import pandas as pd
 import time
 from urllib.parse import urljoin
 
-class WebScrapeBs(ws.WebScrape):
+class WebScrapeBs():
     def __init__(self):
         self.session = requests.Session()
         self.df = pd.DataFrame()
         self.headers = {}
-        
+
+    def clear_df(self):
+        self.df = pd.DataFrame()
+
     def get_element_by_url(self, url, cssselect_expr=None):
         response = self.session.get(url, headers=self.headers)
+        response.encoding = response.apparent_encoding
         time.sleep(0.8)
         soup = BeautifulSoup(response.text, 'lxml')
         for a in soup.find_all('a'):
@@ -25,23 +29,23 @@ class WebScrapeBs(ws.WebScrape):
         else:
             return soup
 
-    def get_url_by_elem(self, element, cssselect_expr_a):
-        for a in element.select(cssselect_expr_a):
-            link_url = a.get('href')
-            yield link_url
+    #def get_url_by_elem(self, element, cssselect_expr_a):
+        #    for a in element.select(cssselect_expr_a):
+        #        link_url = a.get('href')
+        #        yield link_url
 
-    def get_url_by_url(self, url, cssselect_expr_a):
-        element = self.get_element_by_url(url)
-        link_urls = self.get_url_by_elem(element, cssselect_expr_a)
-        for link_url in link_urls:
-            yield link_url
+    #def get_url_by_url(self, url, cssselect_expr_a):
+        #    element = self.get_element_by_url(url)
+        #    link_urls = self.get_url_by_elem(element, cssselect_expr_a)
+        #    for link_url in link_urls:
+        #        yield link_url
 
-    def get_record_by_url(self, url):     
-        element = self.get_element_by_url(url)
-        return self.get_record_by_elem(element)
+    #def get_record_by_url(self, url):     
+        #    element = self.get_element_by_url(url)
+        #    return self.get_record_by_elem(element)
 
-    def get_record_by_elem(self, element):
-        pass
+    #def get_record_by_elem(self, element):
+        #    pass
 
     def scrape_all(self, url):
         pass
